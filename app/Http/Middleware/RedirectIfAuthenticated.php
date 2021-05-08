c3e7123db2666464bdfe95c4172495c11ee8f170
ditrix <dmvoloshin@gmail.com>
2021-05-08 18:52:23 +0300

    redir after auth

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,9 +19,11 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            //return redirect(RouteServiceProvider::HOME);
+            return redirect()->route('user.index');
         }
 
         return $next($request);
+        
     }
 }
